56c1f6be50c1c12e0fa8560dd8051e0604448b73
Francis Lagares <francis.lagares@gmail.com>
2023-08-23 15:10:00 +0200

    feat: create base router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
 
-import App from '@/App';
+import router from './routes';
 
 import '@/css/index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
