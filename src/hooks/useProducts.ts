98c113f857fa50cc9c10c43d0be54c9c160851b3
Francis Lagares <francis.lagares@gmail.com>
2023-08-23 19:35:07 +0200

    feat: create useProducts hook

---
+++ src/hooks/useProducts.ts
@@ -0,0 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
+import ms from 'ms';
+
+import { Product } from '@/models/product';
+import ApiService from '@/services/api.service';
+
+const apiClient = new ApiService<Product>(
+  import.meta.env.VITE_API_PRODUCTS_URL,
+);
+
+const useProducts = () => {
+  return useQuery({
+    queryKey: ['products'],
+    queryFn: apiClient.getProducts,
+    staleTime: ms('24h'),
+  });
+};
+
+export default useProducts;
